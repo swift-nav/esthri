acfbe85642c1b4e1c0d29368e967561d94c75ce0
Jason Mobarak <jason@swiftnav.com>
2020-07-12 15:19:36 -0700

    Release automation

---
+++ scripts/package-version.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+import toml
+
+cargo_toml = toml.load("Cargo.toml")
+version = cargo_toml["package"]["version"]
+
+major, minor, bugfix = map(int, version.split("."))
+
+if len(sys.argv) < 2:
+    sys.stderr.write("ERROR: not enough arguments")
+    sys.exit(1)
+
+if sys.argv[1] == "bump-major":
+    major += 1
+elif sys.argv[1] == "bump-minor":
+    minor += 1
+elif sys.argv[1] == "bump-bugfix":
+    bugfix += 1
+elif sys.argv[1] == "get-current":
+    print(f"{major}.{minor}.{bugfix}")
+    sys.exit(0)
+else:
+    sys.stderr.write("ERROR: unknown command")
+    sys.exit(1)
+
+cargo_toml["package"]["version"] = "{}.{}.{}".format(major, minor, bugfix)
+
+with open("Cargo.toml", "w") as toml_out:
+    toml.dump(cargo_toml, toml_out)
