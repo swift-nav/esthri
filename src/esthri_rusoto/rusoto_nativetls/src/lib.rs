633ff7452b57a66d55486b0b0a6bdcb95aa70cff
Jason Mobarak <jason@swiftnav.com>
2020-11-18 02:22:43 -0800

    headers

--- src/esthri_rusoto/rusoto_nativetls/src/lib.rs
+++ src/esthri_rusoto/rusoto_nativetls/src/lib.rs
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2020 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #[cfg(feature = "nativetls")]
 pub use hyper_tls::{self, *};
 #[cfg(feature = "nativetls")]
