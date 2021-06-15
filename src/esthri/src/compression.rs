4b02f76b2a129395f21a57e9bcd1d42e467b75ff
Jason Mobarak <jason@swiftnav.com>
2021-06-14 18:23:08 -0700

    just use perms of original file

--- src/esthri/src/compression.rs
+++ src/esthri/src/compression.rs
@@ -91,15 +91,8 @@ pub(crate) async fn compress_and_replace(path: impl AsRef<Path>) -> Result<PathB
         .unwrap_or_else(|| path.to_path_buf().with_extension("gz"));
     debug!("renaming {} to {}", path.display(), file_gz.display());
     fs::rename(temp_path, &file_gz)?;
-    #[cfg(unix)]
-    {
-        use std::os::unix::fs::PermissionsExt;
-        let umask = unsafe { libc::umask(0o666) };
-        unsafe { libc::umask(umask) };
-        let mut permissions = file_gz.metadata()?.permissions();
-        permissions.set_mode(0o666 & !umask);
-        std::fs::set_permissions(&file_gz, permissions)?;
-    }
+    let permissions = path.metadata()?.permissions();
+    fs::set_permissions(&file_gz, permissions)?;
     fs::remove_file(path)?;
     Ok(file_gz)
 }
