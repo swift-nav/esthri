0574d11eb3333514ff6b7d7f223fc1c5e07b4217
adrian-kong <adrian.k1234567890@gmail.com>
2022-06-01 09:19:46 +1000

    removed unix import, added naming to file releases

--- crates/esthri-cli/src/main.rs
+++ crates/esthri-cli/src/main.rs
@@ -15,7 +15,6 @@ mod http_server;
 
 use std::env;
 use std::ffi::OsStr;
-use std::os::unix::process::CommandExt;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::time::Duration;
@@ -228,7 +227,7 @@ fn call_real_aws() {
     let aws_tool_path = env::var(REAL_AWS_EXECUTABLE_ENV_NAME)
         .unwrap_or_else(|_| REAL_AWS_EXECUTABLE_DEFAULT.to_string());
 
-    let err = Command::new(&aws_tool_path).args(args).exec();
+    let err = Command::new(&aws_tool_path).args(args).output().err()?;
 
     panic!(
         "Executing aws didn't work. Is it installed and available as {:?}? {:?}",
